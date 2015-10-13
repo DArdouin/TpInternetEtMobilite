d254433405d0ec649b06c147d2291d8215a3e4ad
DArdouin <da.ardouin@gmail.com>
2015-10-13 14:57:31 -0400

    Ajout des classes principales
    Début d'implémentation des principales classes du serveur

---
+++ ServeurHockey/src/Match/Match.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Match;
+
+/**
+ *
+ * @author Damien
+ */
+public class Match {
+    /**
+     * 
+     */
+    Equipe equipe1;
+    /**
+     * 
+     */
+    Equipe equipe2;
+
+    /**
+     * Permet de créer un Match, en passant les équipes en paramètres
+     * @param e1 La première équipe
+     * @param e2 La seconde équipe
+     */
+    public Match(Equipe e1, Equipe e2){
+        equipe1 = e1;
+        equipe2 = e2;
+    }
+}
