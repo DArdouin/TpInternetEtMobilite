4065afb82802f89f5b639f9aecc760e959dfcec6
DHorde <dimitri.horde@gmail.com>
2015-10-20 11:27:46 -0400

    Mise à jour Service

--- Client Android/SoireeHockey/app/src/main/java/com/example/dimitri/soireehockey/ReceiveService.java
+++ Client Android/SoireeHockey/app/src/main/java/com/example/dimitri/soireehockey/ReceiveService.java
@@ -4,7 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
-public class ReceiveService extends Service {
+public class ReceiveService extends IntentService {
     public ReceiveService() {
     }
 
